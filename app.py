61fe6d7a17e068a288e31211fa8586a3a2217352
fitappe123 <brabfblfll@gmail.com>
2025-04-24 10:36:25 +0200

    first commit

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def predict():
 def home():
     return "✅ API is running"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
